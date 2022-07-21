64fb3b9d18ac9d25a995117649e5cd74b35880dd
Justin Bates <jujbates@gmail.com>
2022-07-21 13:43:50 -0700

    back track

--- src/main/java/com/myorg/MyPipelineStepStack.java
+++ src/main/java/com/myorg/MyPipelineStepStack.java
@@ -27,16 +27,16 @@ public class MyPipelineStepStack extends Stack {
         super(scope, id, props);
 
 
-//        Function helloFunction = Function.Builder.create(this, "MyLambdaFunction")
-//
-//                .runtime(Runtime.NODEJS_14_X)
-//                .handler("index.handler")
-//                .code(Code.fromInline(
-//                        "exports.handler = async function(event, context) {\n" +
-//                                "    context.succeed('hello world!');\n" +
-//                                "};"))
-//                .timeout(Duration.seconds(25))
-//                .build();
+        Function helloWorldFunction = Function.Builder.create(this, "MyLambdaFunction")
+
+                .runtime(Runtime.NODEJS_14_X)
+                .handler("index.handler")
+                .code(Code.fromInline(
+                        "exports.handler = async function(event, context) {\n" +
+                                "    context.succeed('hello world!');\n" +
+                                "};"))
+                .timeout(Duration.seconds(25))
+                .build();
 //
 //
 //        List<String> helloWorldPackagingInstructions = Arrays.asList(
@@ -48,14 +48,14 @@ public class MyPipelineStepStack extends Stack {
 //        );
 
 
-        Function helloWorldFunction = new Function(this, "HelloWorld", FunctionProps.builder()
-                .runtime(Runtime.JAVA_11)
-                .code(Code.fromAsset("../lambdas/"))
-                .handler("helloworld.App")
-                .memorySize(1024)
-                .timeout(Duration.seconds(10))
-//                .logRetention(RetentionDays.ONE_WEEK)
-                .build());
+//        Function helloWorldFunction = new Function(this, "HelloWorld", FunctionProps.builder()
+//                .runtime(Runtime.JAVA_11)
+//                .code(Code.fromAsset("../lambdas/"))
+//                .handler("helloworld.App")
+//                .memorySize(1024)
+//                .timeout(Duration.seconds(10))
+////                .logRetention(RetentionDays.ONE_WEEK)
+//                .build());
 
         StateMachine stateMachine = StateMachine.Builder.create(this, "MyStateMachine")
                 .definition(LambdaInvoke.Builder.create(this, "MyLambdaTask")
