cc3e123f499e2639882413878cba80c5ef8c74b1
Justin Bates <jujbates@gmail.com>
2022-07-21 17:05:16 -0700

    Updated with mvn HW Lambda update pipeline stack

--- src/main/java/com/myorg/MyPipelineStepStack.java
+++ src/main/java/com/myorg/MyPipelineStepStack.java
@@ -3,19 +3,20 @@ package com.myorg;
 import java.util.Arrays;
 import java.util.List;
 
-import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.stepfunctions.StateMachine;
 import software.amazon.awscdk.services.stepfunctions.Succeed;
 import software.amazon.awscdk.services.stepfunctions.tasks.LambdaInvoke;
 import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
 
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.s3.assets.AssetOptions;
+
+import static java.util.Collections.singletonList;
+import static software.amazon.awscdk.BundlingOutput.ARCHIVED;
 
 
 public class MyPipelineStepStack extends Stack {
@@ -26,23 +27,37 @@ public class MyPipelineStepStack extends Stack {
     public MyPipelineStepStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-//
-//        Function helloWorldFunction = Function.Builder.create(this, "MyLambdaFunction")
-//
-//                .runtime(Runtime.NODEJS_14_X)
-//                .handler("index.handler")
-//                .code(Code.fromInline(
-//                        "exports.handler = async function(event, context) {\n" +
-//                                "    context.succeed('hello world!');\n" +
-//                                "};"))
-//                .timeout(Duration.seconds(25))
-//                .build();
+
+        List<String> helloWorldFunctionPackagingInstructions = Arrays.asList(
+                "/bin/sh",
+                "-c",
+                "cd lambdas/HWTest " +
+                        "&& mvn clean install " +
+                        "&& cp /asset-input/lambdas/HWTest/target/helloWorldFunction.jar /asset-output/"
+        );
+
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(helloWorldFunctionPackagingInstructions)
+                .image(Runtime.JAVA_11.getBundlingImage())
+                .volumes(singletonList(
+                        // Mount local .m2 repo to avoid download all the dependencies again inside the container
+                        DockerVolume.builder()
+                                .hostPath(System.getProperty("user.home") + "/.m2/")
+                                .containerPath("/root/.m2/")
+                                .build()
+                ))
+                .user("root")
+                .outputType(ARCHIVED);
 
 
 
         Function helloWorldFunction = new Function(this, "helloWorldFunction", FunctionProps.builder()
                 .runtime(Runtime.JAVA_11)
-                .code(Code.fromAsset("lambdas/HWTest/", AssetOptions.builder().build()))
+                .code(Code.fromAsset("lambdas/HWTest/", AssetOptions.builder()
+                        .bundling(builderOptions
+                                .command(helloWorldFunctionPackagingInstructions)
+                                .build())
+                        .build()))
                 .handler("helloworld.App")
                 .build());
 
